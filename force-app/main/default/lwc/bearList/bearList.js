eb6af556b0cd4691ad661356b8f67b29b386be8d
corsawai <handika033@gmail.com>
2022-12-13 08:42:39 -0600

    fix search function

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -2,10 +2,13 @@
 
 import { LightningElement, wire } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
-/** BearController.getAllBears() Apex method */
-import getAllBears from '@salesforce/apex/BearController.getAllBears';
+/** BearController.searchBears(searchTerm) Apex method */
+import searchBears from '@salesforce/apex/BearController.searchBears';
 export default class BearList extends LightningElement {
-	@wire(getAllBears) bears;
+	searchTerm = '';
+	@wire(searchBears, {searchTerm: '$searchTerm'})
+	bears
+
 	appResources = {
 		bearSilhouette: `${ursusResources}/standing-bear-silhouette.png`,
 	};
@@ -22,7 +25,7 @@ export default class BearList extends LightningElement {
 		}, 300);
 	}
 
-	get hasResult(){
+	get hasResults(){
 		return (this.bears.data.length > 0);
 	}
 }
